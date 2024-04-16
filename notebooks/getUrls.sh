497fd26ec278d0b1ee66833f9df7a5f5e13bf172
Alex Owusu <Alexander.c.owusu@gmail.com>
2024-04-16 15:03:07 +0100

    Add script to download and extract files

---
+++ notebooks/getUrls.sh
@@ -0,0 +1,45 @@
+urls  = """
+{
+    "data": {
+        "request_dataset": [
+            "mtsd_fully_annotated_md5_sums.txt => https:\/\/scontent-lhr6-2.xx.fbcdn.net\/m1\/v\/t6\/An8RSqTVAOhQUZHE5NgIL-tgliMqFe4h3ieVXSDNI-ilkh922n_TtB2VpiW-SzAHEYLiqk3Y3FU8pM-DskkOMCvFGh9RJAxxqi3RI2JXDvbQfq4xYwVstPYKlT44GhxzbDbdW_OG0lyY.txt?ccb=10-5&oh=00_AfC0yGAzzuJJAeWoOArGffXI6dFu0ZGhMhnu4_Q3vS8y7Q&oe=6645E505&_nc_sid=6de079",
+            "mtsd_fully_annotated_annotation.zip => https:\/\/scontent-lhr6-2.xx.fbcdn.net\/m1\/v\/t6\/An90x89nHvauCK1fqMJ8110KeTjNo5Si7rzhvwIMCu5xI9_GhWBGOIXaFvu6o53NuNpBMzdC9qsjAVR8sLv8m6WoFfn6Qd4NjMYKNW4NCKVp6gx3MhZtwf3cZR94wFhou5lPI0hGUw.zip?ccb=10-5&oh=00_AfDwXLTgCYvFgjdHs_TqPHGxqfgL5tAxNHmqI2v83WK7mw&oe=66460E88&_nc_sid=6de079",
+            "mtsd_fully_annotated_images.test.zip => https:\/\/scontent-lhr6-2.xx.fbcdn.net\/m1\/v\/t6\/An9eB8zXYW473CiQYW9CPvGx1Ho-fkQvkini3ddExpFOz47aWs4ydBSvK-ZhOPu7ikASQmZvX0zyXhmJzBr6CDZE5ZkUhvJ44h7mV2NT4cSRbR837J9mHJosreQRJJdGaVDR26EAjLPL.zip?ccb=10-5&oh=00_AfCT0Sh4wfuzaTyzo66wMCchN-dYJfsOh21YBrSdsBh0Jw&oe=6645DFDC&_nc_sid=6de079",
+            "mtsd_fully_annotated_images.train.0.zip => https:\/\/scontent-lhr6-2.xx.fbcdn.net\/m1\/v\/t6\/An_WKGcw-ICowA_xAgTEU_E-pAYdybyzT-9Pwi8JtelanWnNRKONV1DTAZPEAsGNDWlFpYDi16km1stDN47ip-quE77cfkv3aERdMIRahysGgspb6DlCgrabPSTFJI3tZ9EMRatRC6ZmjQytVcY.zip?ccb=10-5&oh=00_AfBG-VXzU7jyYRp0EB5hRzRcmugBTOsF6m9R656j9zluFA&oe=6645DC32&_nc_sid=6de079",
+            "mtsd_fully_annotated_images.train.1.zip => https:\/\/scontent-lhr6-2.xx.fbcdn.net\/m1\/v\/t6\/An8VtaI-ldaSOc5HcLFVQ6SPvHDt50hLG1kga0nUfswldLu1J9dsOx6ynZicRUuXR_TvsczpplOqQEa7ppT4JwUzI0ZNQCHmhtkfT5tjdNJY55Ud6eXplvq59PjOx55d2EbIxYpO9vhR-BcflQ.zip?ccb=10-5&oh=00_AfD5P3zXHepp-pFybopZZltW-ArXQR0hvMAsLZA0VB30Ew&oe=6645EE1E&_nc_sid=6de079",
+            "mtsd_fully_annotated_images.train.2.zip => https:\/\/scontent-lhr6-2.xx.fbcdn.net\/m1\/v\/t6\/An9BwhO8zTAy6jGrwy71BjIBtMK8K5RkpIJguP7DVnpJK2TfKDlfxXj8mCxRJss4zzfaaKi2idqbQOtYJ740TPCI7w7hL8V7goknzuO0ZFPLywDCKIB7i64lCiSUNYXLqeS8mC7EkiU5hYAfOcI.zip?ccb=10-5&oh=00_AfBBBZ95PwUiYX4cNvHd-ssD4KLzrmh_m4b38j7K3rJubw&oe=66460FDB&_nc_sid=6de079",
+            "mtsd_fully_annotated_images.val.zip => https:\/\/scontent-lhr6-2.xx.fbcdn.net\/m1\/v\/t6\/An9tl3SwDwRFQ2z9tAag26brYamdQZmHnVoxfNTz_Iass-zZLWM-HryqW44UeqbLWd-EkXVIP-ZQQfg3F7dmQYlnu1wjzCARviaJMBHgtLH4gTAeW6msFbEXA3_NIZBtdP7Gg8dt5Ewl.zip?ccb=10-5&oh=00_AfCL1uzuyuOfsVB-VLg-jORyzurauyjW5FsG44-t5pNbtw&oe=6645F727&_nc_sid=6de079",
+            "mtsd_partially_annotated_md5_sums.txt => https:\/\/scontent-lhr6-2.xx.fbcdn.net\/m1\/v\/t6\/An9FlyymG8PIT47PpdF-es5LyB3G1VvhyKar744ioBXCcFVQDaTVBq1LTvq1vz61u49tefGZb03n-mQ_PWKPIX8ZrMfRI09WMTygDexYMZJ6VkTWJK_7FTY47dC9KeKQG8RG3oGU6Dhw.txt?ccb=10-5&oh=00_AfBLKXR_PnMZKx9t2ZZTPOWrWdC7PnBaRX0cn6RgQRvc4w&oe=6645F683&_nc_sid=6de079",
+            "mtsd_partially_annotated_annotation.zip => https:\/\/scontent-lhr6-2.xx.fbcdn.net\/m1\/v\/t6\/An8dmcHh74W4QOmBjSUnIgjyIDl7pVOj8ym708qotPkG4w_03qYL9KUrHpljvhZgYfNJp82zqzc5ZNAawW6W-huIX9hXtE0Q1wEbbbJWyP2Qgfosi_118TvAPBpKxis6XM8RroqJwg.zip?ccb=10-5&oh=00_AfBoHzc-boDWKCRS-ErgWacYMwRap_GzTswknNP-e5Bv6Q&oe=6645FD10&_nc_sid=6de079",
+            "mtsd_partially_annotated_images.train.0.zip => https:\/\/scontent-lhr6-2.xx.fbcdn.net\/m1\/v\/t6\/An_XCkEh3XjG7bV8c4h5QX-Qodh85URfwXB83u_txLJaEcDmFH6VOUG0O2T8U5WZt7mpEa-EczZiupLH93KsSXfDAIPq8HjPJn1q_oRSBA3ufZus7MnJ_CGs1O166IJiczbnq9J-0NOZ_G5mR8c.zip?ccb=10-5&oh=00_AfDDRNP0HCWwYOJ-ALr0TJTq9T8DIXdiUHVb29D-FHr0vw&oe=6645E080&_nc_sid=6de079",
+            "mtsd_partially_annotated_images.train.1.zip => https:\/\/scontent-lhr6-2.xx.fbcdn.net\/m1\/v\/t6\/An8bwxH70h7HKMm7H2IGV4ptYgE55ozIcf4_wWboduU4ToWxKTnWIelPbSqQ3C1RxKoRTjsOfB9_gzdrv6Oq6qLxEJ1IfOuOXvv6btXEwLfAw2feJOla96rwgmmQyhf-AzgqpS1yIKeydT8zkA.zip?ccb=10-5&oh=00_AfCT2VAjBI-cDr51Fl5ALPpg_utZ4K4Pq_JhZUYYM2VrKQ&oe=6645EA4B&_nc_sid=6de079",
+            "mtsd_partially_annotated_images.train.2.zip => https:\/\/scontent-lhr6-2.xx.fbcdn.net\/m1\/v\/t6\/An-nQsgt_uWGakflQM3LbwL32CaQBwAqrR1r2jdVCHNoR34x1v6LHlvlpTQ9CM86r3tREP05tP049I58J7utUv4vefGA3XD2Up-fJ5cXubNGDCglkw-haZwHnvQR-QtqOisk_5IOaEUK62MYBAE.zip?ccb=10-5&oh=00_AfAhWZ6G4w3o4W775iQLH49qjWgYGmnNr9Xu4C0RJmq1nQ&oe=66460028&_nc_sid=6de079",
+            "mtsd_partially_annotated_images.train.3.zip => https:\/\/scontent-lhr6-2.xx.fbcdn.net\/m1\/v\/t6\/An_v4axbCdfevl9LMrvUjPLJIeyFRpVsQ2RybsjRcTvp835OfXmPuU1Zze2pBLWZ466DP5OHgh2NNayww-Y1za1cXc38w05KcaUPVrkT8mQUv7pvhxnHTit11cPx8wM8ywmWC9H4IZFzfJIMAw.zip?ccb=10-5&oh=00_AfBa5cEHIZcznTxumvDkXTPizgDSnza2iKWJMt9nahTqhA&oe=6645E08D&_nc_sid=6de079"
+        ]
+    },
+    "extensions": {
+        "is_final": true
+    }
+}
+"""
+urls = json.loads(urls)
+urls = urls['data']['request_dataset']
+
+download_dir = 'data'
+for url in urls:
+    filename = url.split(' => ')[0]
+    url = url.split(' => ')[1]
+    print(f"Downloading {filename} from {url}...")
+    !curl -s -o $download_dir/$filename $url
+    print(f"Downloaded {filename} from {url}.")
+    # Check if md5  checksum file
+    if filename.endswith('.txt'):
+        continue
+    print(f"Extracting {filename}...")
+    !unzip -q -d $download_dir/$filename $download_dir/$filename
+    echo "Extracted $filename."
+    echo "Removing $filename..."
+    !rm $download_dir/$filename
+    echo "Removed $filename."
+done
+echo "All downloads and extractions completed."
