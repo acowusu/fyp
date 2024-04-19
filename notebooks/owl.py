8761e860a8bdfa6697647a131a26dcde464ab8e2
Alex Owusu <Alexander.c.owusu@gmail.com>
2024-04-19 16:43:33 +0100

    Add segmentation functionality for traffic sign detection

--- notebooks/owl.py
+++ notebooks/owl.py
@@ -2,9 +2,11 @@ from PIL import Image
 import torch
 import numpy as np
 from transformers import OwlViTProcessor, OwlViTForObjectDetection
+from transformers import AutoImageProcessor, Mask2FormerForUniversalSegmentation
 import json
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import cv2
 
 # objectProcessor = Owlv2Processor.from_pretrained("google/owlv2-base-patch16-ensemble")
 # objectModel = Owlv2ForObjectDetection.from_pretrained("google/owlv2-base-patch16-ensemble")
@@ -12,6 +14,15 @@ import matplotlib.pyplot as plt
 objectProcessor = OwlViTProcessor.from_pretrained("google/owlvit-base-patch32")
 objectModel = OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32")
 
+
+# run_text_extraction_hybrid(0,0,1600,1205)
+
+
+# Load Mask2Former trained on CityScapes panoptic segmentation dataset
+seg_image_processor = AutoImageProcessor.from_pretrained("facebook/mask2former-swin-large-mapillary-vistas-panoptic")
+seg_model = Mask2FormerForUniversalSegmentation.from_pretrained(
+    "facebook/mask2former-swin-large-mapillary-vistas-panoptic"
+)
 key_file = "../ephemeral/sign/mtsd_v2_fully_annotated/splits/train.txt"
 
 with open(key_file, "r") as f:
@@ -80,13 +91,43 @@ def run_object_detection(image, text):
     return boxes
 
 
+def run_segmentation(img):
+    # Preprocess the image
+    inputs = seg_image_processor(images=img, return_tensors="pt")
+
+    # Perform inference
+    with torch.no_grad():
+        return seg_model(**inputs)
+
+def run_segmentation_detection(image, data):
+    interest  = "Traffic Sign (Front)"
+    seg_outputs =  run_segmentation(image)
+
+    pred_panoptic_map = seg_image_processor.post_process_panoptic_segmentation(
+    seg_outputs, target_sizes=[[data["height"], data["width"]]]
+)[0]
+    mask = np.zeros((pred_panoptic_map['segmentation'].shape[0], pred_panoptic_map['segmentation'].shape[1]), dtype=np.uint8) # height, width
+
+    for segment in pred_panoptic_map["segments_info"]:
+        if seg_model.config.id2label[segment["label_id"]] == interest:
+            segment_id = segment['id']
+            mask[np.array(pred_panoptic_map['segmentation']) == segment_id] =   1
+    visual_mask = (mask * 255).astype(np.uint8)
+    contours, hierarchy = cv2.findContours(visual_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    boxes = []
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)  
+        boxes.append([x, y, x+w, y+h])  
+    return boxes
+
 def process_image(key):
     json_file = f"../ephemeral/sign/mtsd_v2_fully_annotated/annotations/{key}.json"
     with open(json_file) as f:
         data = json.load(f)
     image_file = '../ephemeral/sign/images/' + key + '.jpg'
     # image_file = f"../ephemeral/sign/images/{key}.jpg"
-    boxes = run_object_detection(Image.open(image_file), "a traffic sign")
+    # boxes = run_object_detection(Image.open(image_file), "a traffic sign")
+    boxes = run_segmentation_detection(Image.open(image_file), data )
     return iou_numpy(generate_mask_from_json(data), generate_mask_from_boxes(boxes, data["height"], data["width"]))
 
 
@@ -104,7 +145,11 @@ def debug_image(key):
     plt.show()
 
 print(len(keys))
-with open("output.txt", "a") as f:
-    for key in tqdm(keys):
-        result = process_image(key)
-        f.write(f"{key}: {result}\n")
+
+for key in tqdm(keys[0:5]):
+    result = process_image(key)
+    print(result)
+# with open("output2.txt", "a") as f:
+#     for key in tqdm(keys):
+#         result = process_image(key)
+#         f.write(f"{key}: {result}\n")
